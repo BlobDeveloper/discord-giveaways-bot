4582e150a21bafb7369b0d0b7c791bc2a7cb25c5
BlobDev <91900809+BlobDeveloper@users.noreply.github.com>
2022-08-18 12:33:40 +0200

    Update help.js

--- src/Commands/Info/help.js
+++ src/Commands/Info/help.js
@@ -13,7 +13,7 @@ module.exports = {
         .setColor('DarkRed')
         .setThumbnail('https://images.emojiterra.com/twitter/v13.1/512px/1f4cb.png')
         .setTitle('Help Menu')
-        .setDescription(`> You can find all the commands here.\n> In total there are **\`8\`** commands.\nThe source code for this bot is available on [GitHub](https://github.com/BlobDeveloper/discord-giveaway-bot).`)
+        .setDescription(`> You can find all the commands here.\n> In total there are **\`8\`** commands.\nThe source code for this bot is available on [GitHub](https://github.com/BlobDeveloper/discord-giveaways-bot).`)
         .addFields([
             { name: 'ℹ️ Info', value: '**[2]** `help`, `ping`' },
             { name: '🎉 Giveaways', value: '**[6]** `giveaway start`, `giveaway pause`, `giveaway unpause`, `giveaway end`, `giveaway reroll`, `giveaway delete`'}
